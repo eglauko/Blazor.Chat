8462258151fcb73212a165b2637e0083bdd3d003
eglauko <eglauko@hotmail.com>
2019-06-28 00:37:13 -0300

    pu

---
+++ src/Blazor.Chat/Blazor.Chat.Client_old5/BlazorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Blazor
+{
+    public static class BlazorExtensions
+    {
+        public static T Invoke<T>(this IJSRuntime js, string identifier, params object[] args)
+        {
+            return Task.Run(async () => await js.InvokeAsync<T>(identifier, args)).Result;
+        }
+    }
+}
